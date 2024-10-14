c8090662b1bbbcdc375e527fad041215a7b3aa4b
AntoniousNasr <React.3@smartgate-egypt.com>
2024-10-14 10:29:21 -0700

    remove json-server + rename start Icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="app-header">
-      <img src="1_h7GMfvHKvLPkynG9NvULJw.gif" alt="React logo" />
+      <img src="startImg.gif" alt="React logo" />
       <h1>THE REACT QUIZ</h1>
     </header>
   );
