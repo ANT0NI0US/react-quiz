ccc2b6ecece24362f90788052e1505e79d953acf
AntoniousNasr <React.3@smartgate-egypt.com>
2024-10-16 10:00:00 -0700

    reduce the start image size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="app-header">
-      <img src="startImg.gif" alt="React logo" />
+      <img src="https://i.ibb.co/PhDwCL4/startImg.webp" alt="React logo" />
       <h1>THE REACT QUIZ</h1>
     </header>
   );
